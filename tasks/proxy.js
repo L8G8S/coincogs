4da5a99d29e0979fd4c527cd6c569e893f9ea61f
Guillaume Leparmentier <guillaume.leparmentier.daveo@axa.fr>
2017-01-06 18:10:14 +0100

    Reoganize a bit the proxy code

--- tasks/proxy.js
+++ tasks/proxy.js
@@ -4,6 +4,50 @@ var fs = require('fs');
 
 var http = require('http');
 
+function handlePostRequest(request, response) {
+    if (request.method === 'POST') {
+        if(request.url === '/referential/save.svc'){
+            var body = [];
+            request.on('data', function(chunk) {
+                body.push(chunk);
+            }).on('end', function() {
+                body = Buffer.concat(body).toString();
+                gutil.log(body);
+            });
+        
+            request.pipe(response);
+        }
+
+        return true;
+    } 
+
+    return false;
+}
+
+function handleGetRequest(request, response) {
+    if (request.method === 'GET') {
+        /* search filters */
+        if(request.url === '/referential/search_filters.json') {
+            fs.readFile('./data/mock_search_filters.json', function (err, data) {
+                response.setHeader('Content-Type', 'application/json');
+                response.write(data);
+                response.end();
+            });
+        }
+        else if(request.url === '/referential/search_filters_moderne.json') {
+            fs.readFile('./data/mock_search_filters_moderne.json', function (err, data) {
+                response.setHeader('Content-Type', 'application/json');
+                response.write(data);
+                response.end();
+            });
+        }
+
+        return true;
+    }
+
+    return false;
+}
+
 gulp.task('proxy', function () {
     http.createServer(function(request, response) {
         // Set CORS headers
@@ -18,43 +62,14 @@ gulp.task('proxy', function () {
             return;
         }
 
-        // Set routes
-        if (request.method === 'POST') {
+        var handled = false;
+        
+        handled |= handlePostRequest(request, response);;
+        handled |= handleGetRequest(request, response);;
 
-            if(request.url === '/referential/save.svc'){
-                var body = [];
-                request.on('data', function(chunk) {
-                    body.push(chunk);
-                }).on('end', function() {
-                    body = Buffer.concat(body).toString();
-                    gutil.log(body);
-                });
-            
-                request.pipe(response);
-            }
-
-        } 
-        else if (request.method === 'GET') {
-            /* search filters */
-            if(request.url === '/referential/search_filters.json') {
-                fs.readFile('./data/mock_search_filters.json', function (err, data) {
-                    response.setHeader('Content-Type', 'application/json');
-                    response.write(data);
-                    response.end();
-                });
-            }
-            else if(request.url === '/referential/search_filters_moderne.json') {
-                fs.readFile('./data/mock_search_filters_moderne.json', function (err, data) {
-                    response.setHeader('Content-Type', 'application/json');
-                    response.write(data);
-                    response.end();
-                });
-            }
-        }
-        else {
+        if(!handled) {
             response.statusCode = 404;
             response.end();
-        }
-  
+        }  
     }).listen(8080);
 });
